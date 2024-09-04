6c61dca8e54a031a1384a68c754ff042d0e7abea
shrek-with-no-r <cmsarr100@student.hbuhsd.edu>
2024-09-04 10:42:45 -0700

    abcdefghijklmnopqrstuvwxyz

--- problem1.java
+++ problem1.java
@@ -1,1 +1,8 @@
-// Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String food = (" maffe ");
+        System.out.println("I like to eat" + food + "as well");
+    }
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
-// Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String[] args)
+    {
+        String name1 = ("Sigma");
+        String name2 = ("John");
+        String name3 = ("Skibidi");
+        String name4 = ("Sir");
+
+        System.out.print(name4 + " " + name2 + " " + name3 + " " + name1);
+    }
+}
